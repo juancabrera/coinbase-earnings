a7ce74545ae2f335641da20c444a324fb0d735e5
Juan Cabrera <juan.acc@gmail.com>
2021-05-11 15:45:55 -0400

    feat(global): getting earnings of an specific coin

---
+++ earnings.js
@@ -0,0 +1,14 @@
+import { getAccountTotalInvested, getAccountBalance } from './utils.js'
+
+(async () => {
+  let currency = 'ETH'
+  let totalInvested = await getAccountTotalInvested(currency)
+  let accountBalanceResponse = await getAccountBalance(currency)
+  let totalETH = parseFloat(accountBalanceResponse.data.native_balance.amount)
+  console.log(`
+  Currency: ${currency}
+  Invested: $${totalInvested.toFixed(2)}
+  Total: $${totalETH.toFixed(2)}
+  Gains: ${parseFloat((totalETH / totalInvested) * 100).toFixed(2)}%
+  `)
+})()
---
+++ utils.js
@@ -0,0 +1,104 @@
+
+import dotenv from "dotenv"
+import crypto from "crypto"
+import fetch from "node-fetch"
+
+dotenv.config()
+
+const apiKey = process.env.CAK;
+const apiSecret = process.env.CAS;
+let timestamp = Math.floor(Date.now() / 1000);
+
+function getSignature(req) {
+  const message = timestamp + req.method + req.path + req.body;
+  return crypto.createHmac("sha256", apiSecret).update(message).digest("hex")
+}
+export async function listAccounts() {
+  const path = `/v2/accounts?&limit=100&order=asc`
+  const req = {
+    method: 'GET',
+    path: path,
+    body: ''
+  }
+  const signature = getSignature(req)
+  let r = await fetch(`https://api.coinbase.com${path}`,
+  {
+    method: req.method,
+    headers: {
+      'CB-ACCESS-SIGN': signature,
+      'CB-ACCESS-TIMESTAMP': timestamp,
+      'CB-ACCESS-KEY': apiKey
+    }
+  })
+  return r.json()
+}
+
+export async function getWalletAccountID(currency) {
+  let walletAccountID = null;
+  let accounts = await listAccounts()
+  accounts.data.forEach(waid => {
+    if (waid.currency == currency) {
+      walletAccountID = waid.id
+      return walletAccountID
+    }
+  })
+  return walletAccountID
+}
+
+export async function getAccountTransactions(accountID, nextURL) {
+  let path = `/v2/accounts/${accountID}/transactions?&limit=10`
+  if (nextURL != null) {
+    path = nextURL
+  }
+
+  const req = {
+    method: 'GET',
+    path: path,
+    body: ''
+  }
+  const signature = getSignature(req)
+  let r = await fetch(`https://api.coinbase.com${path}`,
+  {
+    method: req.method,
+    headers: {
+      'CB-ACCESS-SIGN': signature,
+      'CB-ACCESS-TIMESTAMP': timestamp,
+      'CB-ACCESS-KEY': apiKey
+    }
+  })
+  return r.json()
+}
+
+export async function getAccountTotalInvested(currency) {
+  let total = 0
+  let accountID = await getWalletAccountID(currency)
+  let transactions = null
+
+  do {
+    transactions = await getAccountTransactions(accountID, transactions != null ? transactions.pagination.next_uri : null)
+    transactions.data.forEach(t => total += parseFloat(t.native_amount.amount))
+  } while (transactions.pagination.next_uri != null)
+
+  return total
+}
+
+export async function getAccountBalance(currency) {
+  let accountID = await getWalletAccountID(currency)
+  let path = `/v2/accounts/${accountID}`
+  const req = {
+    method: 'GET',
+    path: path,
+    body: ''
+  }
+  const signature = getSignature(req)
+  let r = await fetch(`https://api.coinbase.com${path}`,
+    {
+      method: req.method,
+      headers: {
+        'CB-ACCESS-SIGN': signature,
+        'CB-ACCESS-TIMESTAMP': timestamp,
+        'CB-ACCESS-KEY': apiKey
+      }
+    })
+  return r.json()
+}
